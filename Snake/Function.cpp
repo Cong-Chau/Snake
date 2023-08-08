860d9ac8a4e86f2573295bd5b09e3d4546af4e6a
Phan Công Châu <congchau206@gmail.com>
2023-08-08 22:09:57 +0700

    Tạo các hướng di chuyển lên, xuống, trái, phải (tự động chạy đến gameover, chưa thể điều khiển di chuyển được)

--- Snake/Function.cpp
+++ Snake/Function.cpp
@@ -1,5 +1,7 @@
 #include"Header.h"
-
+int wight = 40;
+int heigth = 20;
+int speed = 100;
 
 int Random(int min, int max) {
 	srand(time(NULL));
@@ -15,7 +17,7 @@ void gotoxy(int x, int y) {
 //Khoi tao vi tri dau ran
 void initSnake(Snake& snake) {
 	snake.length = 3;
-	int x = 4, y = 4;
+	int x = 4, y = (heigth / 2) - 1;
 	for (int i = 0; i < snake.length; i++,x--) {
 		snake.point[i].x = x;
 		snake.point[i].y = y;
@@ -32,10 +34,10 @@ bool checkCOOR_Fr_Sn(Snake snake, Coordinates fruit) {
 	}
 	return true;
 }
-void initFruit(Coordinates& fruit) {
+void initFruit(Snake snake, Coordinates& fruit) {
 	fruit.x = Random(1, 18);
 	fruit.y = Random(1, 8);
-	while (!checkCOOR_Fr_Sn) {
+	while (!checkCOOR_Fr_Sn(snake, fruit)) {
 		fruit.x = Random(1, 18);
 		fruit.y = Random(1, 8);
 	}
@@ -44,14 +46,14 @@ void initFruit(Coordinates& fruit) {
 void Map(int score) {
 	system("cls");
 	//Hien thi
-	for (int i = 0; i < 20; i++) {
-		for (int j = 0; j < 10; j++) {
-			if (i == 0 || i == 19) {
+	for (int i = 0; i < wight; i++) {
+		for (int j = 0; j < heigth; j++) {
+			if (i == 0 || i == wight - 1) {
 				gotoxy(i, j);
 				cout << "#";
 				continue;
 			}
-			if (j == 0 || j == 9) {
+			if (j == 0 || j == heigth - 1) {
 				gotoxy(i, j);
 				cout << "#";
 				continue;
@@ -62,9 +64,9 @@ void Map(int score) {
 }
 
 bool checkLose(Snake snake) {
-	if (snake.point[0].x == 0 || snake.point[0].x == 19)
+	if (snake.point[0].x == 0 || snake.point[0].x == wight - 1)
 		return true;
-	if (snake.point[0].y == 0 || snake.point[0].y == 9)
+	if (snake.point[0].y == 0 || snake.point[0].y == heigth - 1)
 		return true;
 	//Chua the text
 	for (int i = 1; i < snake.length; i++) {
@@ -75,11 +77,11 @@ bool checkLose(Snake snake) {
 	return false;
 }
 
-void Move(Snake& snake, Coordinates& fruit, int& score) {
+void Right(Snake& snake, Coordinates& fruit, int& score) {
 	while (1) {
-		for (int x = snake.point[0].x; x < 20; x++) {
+		for (int x = snake.point[0].x; x < wight; x++) {
 			if (checkLose(snake)) {
-				gotoxy(0, 13);
+				gotoxy((wight / 2) - 7, heigth + 3);
 				cout << " GAME OVER!!!\n";
 				return;
 			}
@@ -96,16 +98,192 @@ void Move(Snake& snake, Coordinates& fruit, int& score) {
 			gotoxy(0, 0);
 			if (snake.point[0].x == fruit.x && snake.point[0].y == fruit.y) {
 				snake.length++;
+				initFruit(snake, fruit);
 			}
-			Sleep(60);
+			Sleep(speed);
 			system("cls");
 			Map(score);
 			gotoxy(fruit.x, fruit.y);
 			cout << "o";
+
+			
+		}
+		char _input;
+		int move;
+		while (1) {
+			_input = _getch();
+			move = _input;
+			if (move == 119 || move == 87) {
+				Up(snake, fruit, score);
+			}
+			if (move == 115 || move == 83) {
+				Down(snake, fruit, score);
+			}
+			/*if (move == 100 || move == 68) {
+				Right(snake, fruit, score);
+			}*/
+			if (move == 97 || move == 65) {
+				Left(snake, fruit, score);
+			}
 		}
 	}
 }
 
+void Left(Snake& snake, Coordinates& fruit, int& score) {
+	while (1) {
+		for (int x = snake.point[0].x; x >= 0; x--) {
+			if (checkLose(snake)) {
+				gotoxy((wight / 2) - 7, heigth + 3);
+				cout << " GAME OVER!!!\n";
+				return;
+			}
+			gotoxy(x, snake.point[0].y);
+			for (int i = 0; i < snake.length; i++) {
+				cout << "x";
+				gotoxy(snake.point[i + 1].x, snake.point[i + 1].y);
+			}
+			for (int i = snake.length; i > 0; i--) {
+				snake.point[i].x = snake.point[i - 1].x;
+				snake.point[i].y = snake.point[i - 1].y;
+			}
+			snake.point[0].x = x - 1;
+			gotoxy(0, 0);
+			if (snake.point[0].x == fruit.x && snake.point[0].y == fruit.y) {
+				snake.length++;
+				initFruit(snake, fruit);
+			}
+			Sleep(speed);
+			system("cls");
+			Map(score);
+			gotoxy(fruit.x, fruit.y);
+			cout << "o";
+
+			
+		}
+		char _input;
+		int move;
+		while (1) {
+			_input = _getch();
+			move = _input;
+			if (move == 119 || move == 87) {
+				Up(snake, fruit, score);
+			}
+			if (move == 115 || move == 83) {
+				Down(snake, fruit, score);
+			}
+			if (move == 100 || move == 68) {
+				Right(snake, fruit, score);
+			}
+			/*if (move == 97 || move == 65) {
+				Left(snake, fruit, score);
+			}*/
+		}
+	}
+}
+
+void Down(Snake& snake, Coordinates& fruit, int& score) {
+	while (1) {
+		for (int y = snake.point[0].y; y < heigth; y++) {
+			if (checkLose(snake)) {
+				gotoxy((wight / 2) - 7, heigth + 3);
+				cout << " GAME OVER!!!\n";
+				return;
+			}
+			gotoxy(snake.point[0].x, y);
+			for (int i = 0; i < snake.length; i++) {
+				cout << "x";
+				gotoxy(snake.point[i + 1].x, snake.point[i + 1].y);
+			}
+			for (int i = snake.length; i > 0; i--) {
+				snake.point[i].x = snake.point[i - 1].x;
+				snake.point[i].y = snake.point[i - 1].y;
+			}
+			snake.point[0].y = y + 1;
+			gotoxy(0, 0);
+			if (snake.point[0].x == fruit.x && snake.point[0].y == fruit.y) {
+				snake.length++;
+				initFruit(snake, fruit);
+			}
+			Sleep(speed * 2);
+			system("cls");
+			Map(score);
+			gotoxy(fruit.x, fruit.y);
+			cout << "o";
+
+			
+		}
+		char _input;
+		int move;
+		while (1) {
+			_input = _getch();
+			move = _input;
+			if (move == 119 || move == 87) {
+				Up(snake, fruit, score);
+			}
+			/*if (move == 115 || move == 83) {
+				Down(snake, fruit, score);
+			}*/
+			if (move == 100 || move == 68) {
+				Right(snake, fruit, score);
+			}
+			if (move == 97 || move == 65) {
+				Left(snake, fruit, score);
+			}
+		}
+	}
+}
+
+void Up(Snake& snake, Coordinates& fruit, int& score) {
+	while (1) {
+		for (int y = snake.point[0].y; y >= 0; y--) {
+			if (checkLose(snake)) {
+				gotoxy((wight / 2) - 7, heigth + 3);
+				cout << " GAME OVER!!!\n";
+				return;
+			}
+			gotoxy(snake.point[0].x, y);
+			for (int i = 0; i < snake.length; i++) {
+				cout << "x";
+				gotoxy(snake.point[i + 1].x, snake.point[i + 1].y);
+			}
+			for (int i = snake.length; i > 0; i--) {
+				snake.point[i].x = snake.point[i - 1].x;
+				snake.point[i].y = snake.point[i - 1].y;
+			}
+			snake.point[0].y = y - 1;
+			gotoxy(0, 0);
+			if (snake.point[0].x == fruit.x && snake.point[0].y == fruit.y) {
+				snake.length++;
+				initFruit(snake, fruit);
+			}
+			Sleep(speed * 2);
+			system("cls");
+			Map(score);
+			gotoxy(fruit.x, fruit.y);
+			cout << "o";
+
+			
+		}
+		char _input;
+		int move;
+		while (1) {
+			_input = _getch();
+			move = _input;
+			/*if (move == 119 || move == 87) {
+				Up(snake, fruit, score);
+			}*/
+			if (move == 115 || move == 83) {
+				Down(snake, fruit, score);
+			}
+			if (move == 100 || move == 68) {
+				Right(snake, fruit, score);
+			}
+			if (move == 97 || move == 65) {
+				Left(snake, fruit, score);
+			}
+		}
+	}
+}
 
 void PlayGame() {
 	//Toa do dau ran
@@ -117,7 +295,28 @@ void PlayGame() {
 	// Tao khung tro choi
 	Map(score);
 	initSnake(snake);
-	initFruit(fruit);
-	Move(snake,fruit,score);
-	gotoxy(0, 15);
+	initFruit(snake, fruit);
+	gotoxy(fruit.x, fruit.y);
+
+//	Right(snake, fruit, score);
+	char _input;
+	int move;
+	while (1) {
+		_input = _getch();
+		move = _input;
+		if (move == 119 || move == 87) {
+			Up(snake, fruit, score);
+		}
+		if (move == 115 || move == 83) {
+			Down(snake, fruit, score);
+		}
+		if (move == 100 || move == 68) {
+			Right(snake, fruit, score);
+		}
+		if (move == 97 || move == 65) {
+			Left(snake, fruit, score);
+		}
+	}
+	
+	gotoxy(0, heigth + 5);
 }
--- Snake/Header.h
+++ Snake/Header.h
@@ -3,6 +3,7 @@
 #include<cstdlib>
 #include<ctime>
 #include<Windows.h>
+#include<conio.h>
 using namespace std;
 
 struct Coordinates {
@@ -24,8 +25,11 @@ int Random(int min, int max);
 void gotoxy(int x, int y);
 void initSnake(Snake& snake);
 bool checkCOOR_Fr_Sn(Snake snake, Coordinates fruit);
-void initFruit(Coordinates& fruit);
+void initFruit(Snake snake, Coordinates& fruit);
 void Map(int score);
 bool checkLose(Snake snake);
-void Move(Snake& snake, Coordinates& fruit, int& score);
+void Right(Snake& snake, Coordinates& fruit, int& score);
+void Left(Snake& snake, Coordinates& fruit, int& score);
+void Down(Snake& snake, Coordinates& fruit, int& score);
+void Up(Snake& snake, Coordinates& fruit, int& score);
 void PlayGame();
